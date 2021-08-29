2cd8ddf1fe1e29a9859bdb5aa027c06e4ecf3fd4
Felipe Buso <felipe_buso@hotmail.com>
2021-08-29 18:45:59 -0300

    Updates

---
+++ precache-manifest.8b37d46d411b9d765e2e7cc3261d3686.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3822b5ab026e1bc84353f018fb0ac86f",
+    "url": "/frontend-online-store/index.html"
+  },
+  {
+    "revision": "25d304a1d2bb94a8debc",
+    "url": "/frontend-online-store/static/css/main.3e78d4d8.chunk.css"
+  },
+  {
+    "revision": "5798c4d177ba58d34c48",
+    "url": "/frontend-online-store/static/js/2.d6f83119.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/frontend-online-store/static/js/2.d6f83119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25d304a1d2bb94a8debc",
+    "url": "/frontend-online-store/static/js/main.10061a02.chunk.js"
+  },
+  {
+    "revision": "a2d408d9505863c5fc2c",
+    "url": "/frontend-online-store/static/js/runtime-main.2928fc3e.js"
+  }
+]);
